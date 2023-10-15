7252647b10634a32a47a65e8b428a8fad83b97f0
Tamara1406 <tamaram0053@gmail.com>
2023-10-15 21:50:40 +0200

    Kreirani klijentski kontroler i komunikaciona klasa

---
+++ Client/ClientController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ClientController
+    {
+        public ClientCommunication Communication { get; set; }
+
+        private static ClientController instance;
+        public static ClientController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ClientController();
+                }
+                return instance;
+            }
+        }
+    }
+}
