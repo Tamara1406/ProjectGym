c96d836c04a20bf51ea2a8596473a8ca55b42028
Tamara1406 <tamaram0053@gmail.com>
2023-10-25 22:10:16 +0200

    Dodate broker klase

---
+++ DBBroker/BrokerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBBroker
+{
+    public class BrokerController
+    {
+        private static BrokerController instance;
+        public static BrokerController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BrokerController();
+                }
+                return instance;
+            }
+        }
+
+        Broker broker;
+        public SqlConnection Connection { get; set; }
+        public SqlTransaction Transaction { get; set; }
+        private BrokerController()
+        {
+            broker = new Broker();
+            Connection = broker.Connection;
+            Transaction = broker.Transaction;
+        }
+
+        public void OpenConnection()
+        {
+            Connection.Open();
+        }
+        public void CloseConnection()
+        {
+            Connection.Close();
+        }
+        public void BeginTransaction()
+        {
+            Transaction = Connection.BeginTransaction();
+        }
+        public void CommitTransaction()
+        {
+            Transaction.Commit();
+        }
+        public void RollbackTransaction()
+        {
+            Transaction.Rollback();
+        }
+    }
+}
